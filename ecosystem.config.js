b0e6f9eb3862625e2d1776cc351c0c7324427897
Michael R Creel <michaelrcreel@gmail.com>
2019-03-03 10:29:16 -0600

    Temp

---
+++ ecosystem.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  apps : [{
+    name: 'soccer API',
+    script: './src/server.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: 'one two',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development',
+      GRAPHQL_PORT: 9000,
+      MONGODB_DB: 'ds031257.mlab.com:31257/soccer',
+      MONGODB_USER: 'boss01',
+      MONGODB_PASSWORD: 'boss01'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'node',
+      host : '212.83.163.1',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+}
