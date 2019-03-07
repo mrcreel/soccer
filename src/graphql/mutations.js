6666af15e50d1b37dc8f900871cc881954cd8143
mrcreel <michaelrcreel@gmail.com>
2019-03-07 13:00:46 -0600

    feat: Added deleteLeague function

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -48,6 +48,20 @@ const Mutation = new GraphQLObjectType({
       }
     },
 
+    removeLeague: {
+      type: leagueGraphQLType,
+      args: {
+        id: { type: GraphQLID }
+      },
+      resolve(parent, args) {
+        return League.findByIdAndDelete(args.id).exec()
+          .then(league => league.remove())
+          .then(deletedLeague => deletedLeague)
+          // eslint-disable-next-line no-console
+          .catch(err => console.log(err))
+      }
+    }
+
   }
 })
 
